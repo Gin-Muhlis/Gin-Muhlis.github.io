86bea1b850ac0629ea4a9771d76f3ac156206c51
Gin Gin Nurilham M <ginnncore717@gmail.com>
2022-06-07 06:48:21 +0700

    membuat file message.js dan memperbaiki page contact

---
+++ message.js
@@ -0,0 +1,20 @@
+// Form to googlesheet, button loading effect and alert message
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzcVCkZk4HdpAcrn4tHySufYQLWGcD7RvhKwYrWe27158-MXBcY7UftUQg8i3UoTIAHdA/exec'
+const form = document.forms['form-to-google-sheet'];
+const btn = document.querySelector('.button');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    btn.classList.add('button-loading');
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            console.log('Success!', response);
+            btn.classList.remove('button-loading')
+            form.reset();
+            
+        })
+        .catch(error => console.error('Error!', error.message))
+})
